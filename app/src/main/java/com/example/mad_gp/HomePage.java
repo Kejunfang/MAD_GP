f4a91e9c87ba0dbab7db178b582b504e9c936f3d
hillzhang <vezq52431821z@gmail.com>
2025-12-02 20:06:15 +0800

    renew

--- app/src/main/java/com/example/mad_gp/HomePage.java
+++ app/src/main/java/com/example/mad_gp/HomePage.java
@@ -14,6 +14,5 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
