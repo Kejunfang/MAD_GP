7dd613d92a22301eddb1397d52a4f7febf3c7541
hillzhang <vezq52431821z@gmail.com>
2025-12-03 17:35:15 +0800

    change file name to homepage

--- app/src/main/java/com/example/mad_gp/HomePage.java
+++ app/src/main/java/com/example/mad_gp/HomePage.java
@@ -2,17 +2,13 @@ package com.example.mad_gp;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homepage);
     }
 }
